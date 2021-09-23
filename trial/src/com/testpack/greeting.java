1b7a08173143caed2ec4cf0c28301768f7562bda
shylesh.vijaikumar <shylesh.vijaikumar@N13194.intellectdesign.com>
2021-09-23 11:17:10 +0530

    java first commit

---
+++ trial/src/com/testpack/greeting.java
@@ -0,0 +1,10 @@
+package com.testpack;
+
+public class greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hey there");
+	}
+
+}
